640d9e946b3c593e126a6397ccd43ab0fbf6e573
Juan Galvis <galvisjuanc@gmail.com>
2023-11-08 09:52:47 -0500

    Other part of UnaryOperator

--- modules/src/main/java/com/platzi/functional/_04_functional/StringFunctions.java
+++ modules/src/main/java/com/platzi/functional/_04_functional/StringFunctions.java
@@ -7,6 +7,12 @@ public class StringFunctions {
     public static void main(String[] args) {
         UnaryOperator<String> quote = text -> "\"" + text + "\"";
 
+        UnaryOperator<String> addMark = text -> text + "!";
+
         System.out.println(quote.apply("Hola Estudiante de platzi"));
+
+        System.out.println(addMark.apply("Natalie bella"));
+
+
     }
 }
