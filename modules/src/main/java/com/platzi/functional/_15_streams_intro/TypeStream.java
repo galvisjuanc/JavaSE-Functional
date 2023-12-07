568ec4e63739ef305da5c5ca4e404a308f574391
Juan Galvis <galvisjuanc@gmail.com>
2023-12-07 16:54:12 -0500

    Streams with parallel

--- modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
+++ modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
@@ -6,6 +6,7 @@ public class TypeStream {
     public static void main(String[] args) {
         IntStream infiniteStream = IntStream.iterate(0, x -> x + 1);
         infiniteStream.limit(1000)
+                .parallel()
                 .filter(x -> x % 2 == 0)
                 .forEach(System.out::println);
     }
